8f0ea1e29ac218647ed2a53a3583c744f88f2397
youth <337496746@qq.com>
2020-11-21 22:41:52 +0800

    first commit

---
+++ src/test/java/com/wjx/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjx.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
